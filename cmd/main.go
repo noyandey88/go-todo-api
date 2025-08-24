78fd53eb6f7ed65036f0d928e62caaed5567ea50
noyandey88 <coder.noyan@gmail.com>
2025-08-24 23:08:40 +0600

    feat: update swagger base url

--- cmd/main.go
+++ cmd/main.go
@@ -18,7 +18,7 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost:8080
+// @host go-todo-api-36ta.onrender.com
 // @BasePath /api
 
 // @securityDefinitions.apikey BearerAuth
