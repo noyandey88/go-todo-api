bd3986ed2895da41e28cae35c529a33c81f91f04
noyandey88 <coder.noyan@gmail.com>
2025-08-24 23:15:50 +0600

    feat: updated swagger docs

--- docs/docs.go
+++ docs/docs.go
@@ -904,7 +904,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "go-todo-api-36ta.onrender.com",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "Go Todos API",
